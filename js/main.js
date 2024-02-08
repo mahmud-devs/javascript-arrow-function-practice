54a69133214cf44b48a2a2117ce262d916466503
mahmud-devs <mahmudulislam0999@gmail.com>
2024-02-08 19:21:05 +0600

    .

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*
+
+================= arrow function ============
+
+*/
+
+const sum = (num1, num2) => {
+    return ("This is sum:", num1 + num2);
+};
+const subtract = (num1, num2) => {
+    return  num1 - num2;
+
+};
+const multiply = (num1, num2) => {
+    return  num1 - num2;
+
+};
+const division = (num1, num2) => {};
+const modulas = (num1, num2) => {};
+const exponential = (num1, num2) => {};
+
+// =================== variables , array , loop==================
+
+let allFunction = [sum, subtract, multiply, division, modulas, exponential];
+
+let num1 = 40;
+let num2 = 13;
+
+for (i = 0; i < allFunction.length; i++) {
+    let allResult = allFunction[i](num1, num2);
+    console.log(allResult);
+}
