e9413ce853253811fa4fbefd21cd924a2582c349
mahmud-devs <mahmudulislam0999@gmail.com>
2024-02-08 19:30:55 +0600

    if else used

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ const subtract = (num1, num2) => {
 const multiply = (num1, num2) => {
     return num1 * num2;
 };
-const division = (num1, num2) => {
+const divison = (num1, num2) => {
     return num1 / num2;
 };
 const modulas = (num1, num2) => {
@@ -25,12 +25,30 @@ const exponential = (num1, num2) => {
 
 // =================== variables , array , loop==================
 
-let allFunction = [sum, subtract, multiply, division, modulas, exponential];
+let allFunction = [sum, subtract, multiply, divison, modulas, exponential];
 
 let num1 = 40;
 let num2 = 13;
 
 for (i = 0; i < allFunction.length; i++) {
     let allResult = allFunction[i](num1, num2);
-    console.log(allResult);
+    if (allFunction[i] == allFunction[0]) {
+        console.log("This is sum:", allResult);
+    }
+    if (allFunction[i] == allFunction[1]) {
+        console.log("This is subtract:", allResult);
+    }
+    if (allFunction[i] == allFunction[2]) {
+        console.log("This is multiply:", allResult);
+    }
+    if (allFunction[i] == allFunction[3]) {
+        console.log("This is divison:", allResult);
+    }
+    if (allFunction[i] == allFunction[4]) {
+        console.log("This is modulas:", allResult);
+    }
+    if (allFunction[i] == allFunction[5]) {
+        console.log("This is exponential:", allResult);
+    } else {
+    }
 }
