902c5b6ed278a4d2dba5f229d56e783daa517a41
mahmud-devs <mahmudulislam0999@gmail.com>
2024-02-11 23:30:34 +0600

    '

--- js/main.js
+++ js/main.js
@@ -4,23 +4,6 @@
 
 */
 
-const sum = (num1, num2) => {
-    return "This is sum:", num1 + num2;
-};
-const subtract = (num1, num2) => {
-    return num1 - num2;
-};
-const multiply = (num1, num2) => {
-    return num1 * num2;
-};
-const divison = (num1, num2) => {
-    return num1 / num2;
-};
-const modulas = (num1, num2) => {
-    return num1 % num2;
-};
-const exponential = (num1, num2) => {
-    return num1 ** num2;
 };
 
 // =================== variables , array , loop==================
