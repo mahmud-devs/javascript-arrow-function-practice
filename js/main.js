a2332733bd39b7af393535d6485882bd6b579b76
mahmud-devs <mahmudulislam0999@gmail.com>
2024-02-11 23:09:23 +0600

    '

--- js/main.js
+++ js/main.js
@@ -28,3 +28,27 @@ const exponential = (num1, num2) => {
 let allFunction = [sum, subtract, multiply, divison, modulas, exponential];
 
 let num1 = 40;
+let num2 = 13;
+
+for (i = 0; i < allFunction.length; i++) {
+    let allResult = allFunction[i](num1, num2);
+    if (allFunction[i] == allFunction[0]) {
+        console.log("This is sum:", allResult);
+    }
+    if (allFunction[i] == allFunction[1]) {
+        console.log("This is subtract:", allResult);
+    }
+    if (allFunction[i] == allFunction[2]) {
+        console.log("This is multiply:", allResult);
+    }
+    if (allFunction[i] == allFunction[3]) {
+        console.log("This is divison:", allResult);
+    }
+    if (allFunction[i] == allFunction[4]) {
+        console.log("This is modulas:", allResult);
+    }
+    if (allFunction[i] == allFunction[5]) {
+        console.log("This is exponential:", allResult);
+    } else {
+    }
+}
